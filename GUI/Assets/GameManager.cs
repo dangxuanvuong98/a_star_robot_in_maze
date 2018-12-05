cdf53999eabf7cdd2f6a6c697b4cc997e2b17bd2
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-12-05 14:47:36 +0700

    hihi

---
+++ GUI/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        ReadString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    static string ReadString()
+    {
+        string path = "F:/testing.txt";
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string temp = reader.ReadToEnd().ToString();
+        reader.Close();
+        return temp;
+    }
+}
